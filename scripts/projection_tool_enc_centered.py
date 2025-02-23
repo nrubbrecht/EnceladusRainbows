334fc8bbe9505f076506e5efe88780b8c4d1dc19
nrubbrecht <n.rubbrecht@student.tudelft.nl>
2025-02-23 14:32:37 +0100

    created index.html

--- scripts/projection_tool_enc_centered.py
+++ scripts/projection_tool_enc_centered.py
@@ -650,7 +650,7 @@ def plot_spice_scene_animated(spectrum_str, ice_model_str, flyby_name, hours_fro
         }], scene_camera=camera
     )
     # write html
-    fig.write_html("../docs/enc_tool_showcase.html")
+    fig.write_html("../docs/index.html")
 
     # Show figure
     fig.show()
